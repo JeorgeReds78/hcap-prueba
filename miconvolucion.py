695ed1cf2aea2c22b516a268550adeaf2a3a83e9
root <root@DESKTOP-U2JL78S.localdomain>
2020-10-28 15:05:54 -0600

    EL EJERCICIO

--- miconvolucion.py
+++ miconvolucion.py
@@ -5,7 +5,11 @@ def convolucion(A, B):
     C = np.zeros((len(A)-2, len(A[0])-2))
     for i in range(0, len(A)-2):
         for j in range(0, len(A[0])-2):
-            C[i][j] = int(A[i][j]*B[0][0] + A[i][j+1]*B[0][1] + A[i][j+2]*B[0][2] + A[i+1][j]*B[1][0] + A[i+1][j+1]*B[1][1] + A[i+1][j+2]*B[1][2] + A[i+2][j]*B[2][0] + A[i+2][j+1]*B[2][1] + A[i+2][j+2]*B[2][2])
+            suma = 0
+            for x in range(0,3):
+                for y in range(0,3):
+                    suma = suma + A[i+x][j+y]*B[x][y]
+            C[i][j] = suma
     return C
 
 Matriz1 = [[6,9,0,3],[8,4,9,1],[4,1,3,12],[3,2,1,100]]
